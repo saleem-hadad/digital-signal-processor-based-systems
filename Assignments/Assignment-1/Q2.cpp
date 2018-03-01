09c670ac72f021cc1dd1218524169fbe0d67e2c5
Saleem Hadad <saleem_ha@hotmail.com>
2018-03-01 10:29:20 +0800

    update assignment 1

--- Assignments/Assignment-1/Q2.cpp
+++ Assignments/Assignment-1/Q2.cpp
@@ -3,13 +3,14 @@ int main()
 	int input=0, output=0, stageA=0, stageB=0;
 	
 	Analog analog;
-	Delay delays[2];
+	Delay delays[3];
 
 	while(1)
 	{
 		input = analog.read();
-		stageA = delays[0].process(output);
-		stageB = 0.5 * (stageA + delays[1].process(stageA));
+		input = 2 * delays[0].process(input) + input;
+		stageA = delays[1].process(output);
+		stageB = 1.5 * stageA - 0.9 * delays[2].process(stageA);
 		output = input + stageB;
 		analog.write(output);
 	}
