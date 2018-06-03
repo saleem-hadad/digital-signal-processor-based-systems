5aae4a7cfd6218d72251ae09c05c7c2f5ae91133
Saleem Hadad <saleem_ha@hotmail.com>
2018-06-03 16:57:20 +0800

    add DFT

---
+++ practice/DFT.py
@@ -0,0 +1,32 @@
+from math import *
+
+class Complex:
+	def __init__(self, real, imaginary):
+		self.real = real
+		self.imaginary = imaginary
+
+def IDFT(input):
+	n = len(input)
+	k = n / 2 + 1
+
+	freqDomainSignal = []
+	for i in range(k):
+		real = 0
+		imaginary = 0
+		for j in range(n):
+			real = real + input[j] * cos((2 * pi * i * j)/n)
+			imaginary = imaginary - input[j] * sin((2 * pi * i * j)/n)
+
+		freqDomainSignal.append(Complex(real, imaginary))
+
+	return freqDomainSignal
+
+input = [0.0, 1.0, 1.0, 1.0] # -> (3, 0) (-1, 0) (-1, 0)
+output = IDFT(input) 
+for wave in output:
+	print(wave.real, wave.imaginary)
+
+input = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0] # -> (15 + 0j) (−3 + 5.196153𝑗) (−3 + 1.732051𝑗) (−3 +0j)
+output = IDFT(input) 
+for wave in output:
+	print(wave.real, wave.imaginary)
